3b9a98255b041550f52a799234b5e56bf7e3cdec
Alek <alekvalencia2535@gmail.com>
2020-06-10 22:19:03 -0700

    added classes to all the position files with constructors and methods. also made sure to export at bottom of the page. thay all passed the tests in test folder.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
+const Employee = require('./Employee');
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(name, id, email, GitHubUser) {
+        super(name, id, email);
+        this.github = GitHubUser;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;
