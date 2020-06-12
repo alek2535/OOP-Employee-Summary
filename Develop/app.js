a37f7111d2616a038228b1323ee3a1a6bec285af
Alek <alekvalencia2535@gmail.com>
2020-06-11 23:45:24 -0700

    had to restructuremy app.js file. added objects with questions instead of putting it all in one function. Made one for each role, general questions, and if you want to add more team members. switched from promises to async await. condensed and got conditional statement to work for additional questions for each role. added conditional statement for adding another team member and added render function to produce html for team page. also removed userPrompt() and only using init() for now.

--- Develop/app.js
+++ Develop/app.js
@@ -10,96 +10,140 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeesArr = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const userPrompt = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the employee?',
-            validation: answer => {
-                if (answer.length < 1) {
-                    return 'You need to enter a name for the employee.'
-                } return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is their ID?',
-            validation: answer => {
-                if (answer.length < 1) {
-                    return 'You need to enter an ID for the employee.'
-                } return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the email for this employee?',
-            validation: answer => {
-                if (answer.length < 1) {
-                    return 'You need to enter an email for the employee.'
-                } return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'role',
-            message: 'What is the role of the employee?',
-            validation: answer => {
-                if (answer.length < 1) {
-                    return 'You need to enter a role for the employee.'
-                } return true;
-            }
+const managerRole = 
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is their office number?',
+        validate: answer => {
+            if (answer.length < 1) {
+                return 'You need to enter an office number.'
+            } return true;
         }
-    ])
-        .then(answers => {
-            if (answers.role === 'Manager') {
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'officeNumber',
-                        message: 'What is their office number?',
-                        validation: answer => {
-                            if (answer.length < 1) {
-                                return 'You need to enter an office number.'
-                            } return true;
-                        }
-                    }
-                ])
-            } else if (answers.role === 'Engineer') {
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'github',
-                        message: 'What is their GitHub user name?',
-                        validation: answer => {
-                            if (answer.length < 1) {
-                                return 'You need to enter an their username.'
-                            } return true;
-                        }
-                    }
-                ])
-            } else if (answers.role === 'Intern') {
-                return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'school',
-                        message: 'What school do they attend?',
-                        validation: answer => {
-                            if (answer.length < 1) {
-                                return 'You need to enter a school.'
-                            } return true;
-                        }
-                    }
-                ])
-            }
-        })
+    };
+
+const engineerRole = 
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is their GitHub user name?',
+        validate: answer => {
+            if (answer.length < 1) {
+                return 'You need to enter an their username.'
+            } return true;
+        }
+    };
+
+const internRole = 
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school do they attend?',
+        validate: answer => {
+            if (answer.length < 1) {
+                return 'You need to enter a school.'
+            } return true;
+        }
+    };
+
+const employeeQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?',
+        validate: answer => {
+            if (answer.length < 1) {
+                return 'You need to enter a name for the employee.';
+            } return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is their ID?',
+        validate: answer => {
+            if (answer.length < 1) {
+                return 'You need to enter an ID number for the employee.';
+            } return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email for this employee?',
+        validate: answer => {
+            if (answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+                return true;
+            } else {
+                return 'Invalid email. Please enter valid email';
+            };
+        }
+    },
+    {
+        type: 'rawlist',
+        name: 'role',
+        message: 'What is the role of the employee?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    }
+];
+
+const anotherEmployee = 
+    {
+        type: 'confirm',
+        name: 'another',
+        message: 'Do you want to add another team member?'
+    };
+
+
+const init = async () => {
+    console.log('Hello! Please answer the following questions to build your team.');
+
+    const generalQ = await inquirer.prompt(employeeQuestions);
+    const {name, id, email, role} = generalQ;
+
+    if (role === 'Manager') {
+        const managerQ = await inquirer.prompt(managerRole);
+        const office = managerQ.officeNumber;
+
+        const newManager = new Manager(name, id, email, office);
+        employeesArr.push(newManager);
+    } else if (role === 'Engineer') {
+        const engineerQ = await inquirer.prompt(engineerRole);
+        const gitHub = engineerQ.github;
+
+        const newEngineer = new Engineer(name, id, email, gitHub);
+        employeesArr.push(newEngineer);
+    } else if (role === 'Intern') {
+        const internQ = await inquirer.prompt(internRole);
+        const school = internQ.school;
+
+        const newIntern = new Intern(name, id, email, school);
+        employeesArr.push(newIntern);
+    }
+
+    const newEmployee = await inquirer.prompt(anotherEmployee);
+    const yesOrNo = newEmployee.another;
+
+    if (yesOrNo) {
+        init();
+    } else {
+        console.log('You have succesfully built your team!');
+        console.log(employeesArr);
+
+        const teamList = render(employeesArr);
+        fs.writeFile(outputPath, teamList, error => console.log(error));
+    }
+    
 }
 
+        
+
+
+init();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
