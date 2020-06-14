6c4e62424274ac4b56f54e5afd5b11c609fb9459
Alek <alekvalencia2535@gmail.com>
2020-06-13 20:16:41 -0700

    added classes to html pages to stylize pages and output.

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//Empty array for team objects
 const employeesArr = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Manager specific questions for inquier.prompt
 const managerRole = 
     {
         type: 'input',
@@ -26,6 +26,7 @@ const managerRole =
         }
     };
 
+// Engineer specific questions for inquier.prompt
 const engineerRole = 
     {
         type: 'input',
@@ -38,6 +39,7 @@ const engineerRole =
         }
     };
 
+// Intern specific questions for inquier.prompt
 const internRole = 
     {
         type: 'input',
@@ -50,6 +52,7 @@ const internRole =
         }
     };
 
+// General employee questions for inquier.prompt
 const employeeQuestions = [
     {
         type: 'input',
@@ -91,6 +94,7 @@ const employeeQuestions = [
     }
 ];
 
+// Question to add another employee
 const anotherEmployee = 
     {
         type: 'confirm',
@@ -99,32 +103,40 @@ const anotherEmployee =
     };
 
 
+
+// Init function to start inquirer.prompt
 const init = async () => {
     console.log('Hello! Please answer the following questions to build your team.');
 
     const generalQ = await inquirer.prompt(employeeQuestions);
+    //Creates object for classes
     const {name, id, email, role} = generalQ;
 
+    //Conditional statement to get to role specific questions
     if (role === 'Manager') {
         const managerQ = await inquirer.prompt(managerRole);
         const office = managerQ.officeNumber;
 
+        // Creates object for Manager
         const newManager = new Manager(name, id, email, office);
         employeesArr.push(newManager);
     } else if (role === 'Engineer') {
         const engineerQ = await inquirer.prompt(engineerRole);
         const gitHub = engineerQ.github;
 
+        // Creates object for Engineer
         const newEngineer = new Engineer(name, id, email, gitHub);
         employeesArr.push(newEngineer);
     } else if (role === 'Intern') {
         const internQ = await inquirer.prompt(internRole);
         const school = internQ.school;
 
+        // Creates object for Intern
         const newIntern = new Intern(name, id, email, school);
         employeesArr.push(newIntern);
     }
 
+    // Prompt to add more employees
     const newEmployee = await inquirer.prompt(anotherEmployee);
     const yesOrNo = newEmployee.another;
 
@@ -133,9 +145,12 @@ const init = async () => {
     } else {
         console.log('You have succesfully built your team!');
         console.log(employeesArr);
-
+        // After the user has input all employees desired, call the `render` function and pass in an array containing all employee objects
         const teamList = render(employeesArr);
-        fs.writeFile(outputPath, teamList, error => console.log(error));
+        // Create an HTML file using the HTML returned from the `render` function using the variable `outputPath` above.
+        fs.writeFile(outputPath, teamList, error => {
+            if (error) throw error;
+        })
     }
     
 }
@@ -145,22 +160,3 @@ const init = async () => {
 
 init();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
